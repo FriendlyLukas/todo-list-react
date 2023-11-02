a945720ad796134ea8398af3a5595b2145516dcf
Łukasz Palow <lukaspalow@gmail.com>
2023-11-02 19:01:23 +0100

    Tasks index update

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,21 +3,22 @@ import "./style.css";
 const Tasks = (props) => (
   <ul className="tasks">
     {props.tasks.map((task) => (
-      <div key={task.id}>
-        <li
-          className={` tasks__item 
+      <li
+        key={task.id}
+        className={` tasks__item 
           ${
-            task.done & props.hideDoneTasksParameter ? "tasks__item--hidden" : ""
+            task.done & props.hideDoneTasksParameter
+              ? "tasks__item--hidden"
+              : ""
           } 
           ${task.done ? "tasks__item--done" : ""}`}
-        >
-          <button className="tasks__taskButton">{task.done ? "✔" : ""}</button>
-          <span className="tasks__taskContent">{task.content}</span>
-          <button className="tasks__taskButton tasks__taskButton--remove">
-            🗑
-          </button>
-        </li>
-      </div>
+      >
+        <button className="tasks__taskButton">{task.done ? "✔" : ""}</button>
+        <span className="tasks__taskContent">{task.content}</span>
+        <button className="tasks__taskButton tasks__taskButton--remove">
+          🗑
+        </button>
+      </li>
     ))}
   </ul>
 );
