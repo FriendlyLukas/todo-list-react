7a1f218a0eb07675e872d454d0b18374a14f78f5
Łukasz Palow <lukaspalow@gmail.com>
2023-11-07 19:31:50 +0100

    hide done tasks interaction

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ import ManageButtons from "./ManageButtons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "Przejść na React", done: false },
   { id: 2, content: "Pograć w Jenga", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
   return (
     <Container>
       <Header title={"Lista zadań"} />
       <Section title={"Dodaj nowe zadanie"} body={<Form />} />
       <Section
         title={"Lista zadań"}
-        extraHeaderContent={<ManageButtons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <ManageButtons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
+        }
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
       />
     </Container>
--- src/ManageButtons/index.js
+++ src/ManageButtons/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-const ManageButtons = ({ hideDone, tasks }) => (
+const ManageButtons = ({ hideDone, tasks, toggleHideDone }) => (
   <div className="tasks__header">
     {tasks.length > 0 && (
       <>
-        <button className="tasks__manageButton">
+        <button onClick={toggleHideDone} className="tasks__manageButton">
           {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
         </button>
         <button
