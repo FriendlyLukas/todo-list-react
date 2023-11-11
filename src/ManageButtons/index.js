cb61d4862cc53a8ec5f517247a03a1b70bd5d9f2
Łukasz Palow <lukaspalow@gmail.com>
2023-11-11 09:05:27 +0100

    setAllDone interaction

--- src/ManageButtons/index.js
+++ src/ManageButtons/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const ManageButtons = ({ hideDone, tasks, toggleHideDone }) => (
+const ManageButtons = ({ hideDone, tasks, toggleHideDone, setAllDone }) => (
   <div className="tasks__header">
     {tasks.length > 0 && (
       <>
@@ -8,6 +8,7 @@ const ManageButtons = ({ hideDone, tasks, toggleHideDone }) => (
           {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
         </button>
         <button
+          onClick={setAllDone}
           className="tasks__manageButton"
           disabled={tasks.every(({ done }) => done)}
         >
